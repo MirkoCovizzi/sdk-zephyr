8995d251cdfe6ce7348a5e5950b7a82b5aa927a2
Mirko Covizzi <mirko.covizzi@nordicsemi.no>
2024-01-12 17:21:22 +0100

    net: sockets: prevent null pointer dereference
    This commit adds a check to prevent a null pointer
    dereference inside `z_impl_zsock_accept`.
    According to the POSIX specification, null
    is a valid value for the `address` argument.
    
    Signed-off-by: Mirko Covizzi <mirko.covizzi@nordicsemi.no>

--- subsys/net/lib/sockets/sockets.c
+++ subsys/net/lib/sockets/sockets.c
@@ -684,7 +684,11 @@ int z_impl_zsock_accept(int sock, struct sockaddr *addr, socklen_t *addrlen)
 
 	new_sock = VTABLE_CALL(accept, sock, addr, addrlen);
 
-	(void)sock_obj_core_alloc_find(sock, new_sock, addr->sa_family, SOCK_STREAM);
+	if (addr) {
+		(void)sock_obj_core_alloc_find(sock, new_sock, addr->sa_family, SOCK_STREAM);
+	} else {
+		(void)sock_obj_core_alloc_find(sock, new_sock, 0, SOCK_STREAM);
+	}
 
 	return new_sock;
 }
